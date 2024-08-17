1cdc893239fd72263b2d083560873c1f7e976cb9
Apple <apple@MacBook-Pro-Apple.local>
2024-08-17 16:50:42 +0300

    исправлена логика работы с отрицательными числами

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,8 +1,6 @@
 export function parseExpression(expression: string): string[] {
-  // Используем match для разбиения строки на токены
-  const tokens = expression.match(/[\d\.]+|[()+\-*/√%]/g);
+  const tokens = expression.match(/(\d+(\.\d+)?|\+|\-|\*|\/|\%|\(|\)|√)/g);
 
-  // Проверяем, что результат не null
   if (tokens === null) {
     throw new Error("Invalid Expression");
   }
